56c5e55c160d82b6c17c203f83d77837f19df911
Hristo Papucharov <hpapucharov@gmail.com>
2021-04-02 20:04:54 -0500

    Indentation fix

--- src/main/java/com/springhw/demo/controller/AppController.java
+++ src/main/java/com/springhw/demo/controller/AppController.java
@@ -1,4 +1,6 @@
-package Spring;
+package com.springhw.demo.controller;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,6 @@ public class AppController {
     //When the user visits http:/localhost/9092/hello
     @GetMapping("/hello")
     public String helloWorld(){
-        return "Hello world";
+        return "Hello world from homework commit 1";
     }
 }
