49e9bd62edeccf603982f5e1999c8cfb28e66bfd
Hristo Papucharov <hpapucharov@gmail.com>
2021-04-05 19:23:02 -0500

    Delete method

--- src/main/java/com/springhw/demo/controller/Controller.java
+++ src/main/java/com/springhw/demo/controller/Controller.java
@@ -1,13 +1,10 @@
 package com.springhw.demo.controller;
-
-
 import com.springhw.demo.exception.InformationExistException;
 import com.springhw.demo.exception.InformationNotFoundException;
 import com.springhw.demo.model.Category;
 import com.springhw.demo.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -82,9 +79,16 @@ public class Controller {
 
     // http://localhost:PORT-NUMBER/api/categories/1
     @DeleteMapping(path = "/categories/{categoryId}")
-    public String deleteCategory(){
+    public Optional<Category> deleteCategory(@PathVariable(value = "categoryId") Long categoryId) {
         System.out.println("calling deleteCategory ==>");
-        return "delete a single category";
+        Optional<Category> category = categoryRepository.findById(categoryId);
+        if (category.isPresent()) {
+            categoryRepository.deleteById(categoryId);
+            return category;
+        } else {
+            throw new InformationNotFoundException("category with id " + categoryId + " not found");
+        }
+
     }
 
 }
